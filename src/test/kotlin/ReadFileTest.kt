9961326f055fa90c1dfe7272eb822b0759da54b2
KenDahle <ken.dahle@nav.no>
2023-06-13 14:33:27 +0200

    Nais lese fra resource

--- src/test/kotlin/ReadFileTest.kt
+++ src/test/kotlin/ReadFileTest.kt
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test
 import sokos.skd.poc.readFileFromOS
+import java.net.URL
 
 class ReadFileTest {
 
     @Test
     fun lesinnHeleFila() {
-        var liste = readFileFromOS("/Users/d149678/Documents/jobb/SKATT-POC/eksempelfil_TBK.txt")
+        var liste = readFileFromOS("eksempelfil_TBK.txt".asResource().path.also { println(it) })
         println("Antall i lista ${liste.size}")
         println("Første linje: ${liste.first()}")
         println("Siste linje: ${liste.last()}")
@@ -19,3 +20,5 @@ class ReadFileTest {
         return filLinjer.last()
     }
 }
+
+fun String.asResource(): URL = object {}.javaClass.classLoader.getResource(this)!!
