0d200f70d5249a256f2b551efaa4546725ac8647
KenDahle <ken.dahle@nav.no>
2023-08-18 19:15:25 +0200

    TOB-3018 funka ikke med ->

--- src/main/kotlin/sokos/skd/poc/maskinporten/MaskinportenAccessTokenClient.kt
+++ src/main/kotlin/sokos/skd/poc/maskinporten/MaskinportenAccessTokenClient.kt
@@ -41,7 +41,7 @@ class MaskinportenAccessTokenClient(
     }
 
     private suspend fun hentAccessTokenFraProvider(): Token {
-        println { "hentAccessToken ${maskinportenConfig.openIdConfiguration.issuer}, ${maskinportenConfig.clientId}, ${maskinportenConfig.scopes}" }
+        println( "hentAccessToken ${maskinportenConfig.openIdConfiguration.issuer}, ${maskinportenConfig.clientId}, ${maskinportenConfig.scopes}" )
         val jwt = JWT.create()
             .withAudience(maskinportenConfig.openIdConfiguration.issuer)
             .withIssuer(maskinportenConfig.clientId)
