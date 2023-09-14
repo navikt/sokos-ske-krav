0128743f3cd30e54bbd72a4a5b1b9bcd30996bd7
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2023-09-14 08:51:43 +0200

    Bytter tilbake til navikt docker image

--- setupLocalEnvironment.sh
+++ setupLocalEnvironment.sh
@@ -9,9 +9,9 @@ kubectl config use-context dev-fss
 kubectl config set-context --current --namespace=okonomi
 
 # Get AZURE system variables
-envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-ske-krav | cut -f1 -d' ') -c sokos-ske-krav -- env | egrep "^MASKINPORTEN|^ske_REST_URL|^FTP")
+envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-ske-krav | cut -f1 -d' ') -c sokos-ske-krav -- env | egrep "^MASKINPORTEN|^SKE_REST_URL|^FTP")
 
 # Set AZURE as local environment variables
 rm -f defaults.properties
 echo "$envValue" > defaults.properties
-echo "MASKINPORTEN, ske_REST_URL, FTP stored as defaults.properties"
+echo "MASKINPORTEN, SKE_REST_URL, FTP stored as defaults.properties"
