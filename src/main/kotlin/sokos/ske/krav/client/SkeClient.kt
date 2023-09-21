8998ff8cefbb9e935a10449d8cb4db8a3bc14c68
KenDahle <ken.dahle@nav.no>
2023-09-21 08:34:23 +0200

    TOB-3018 litt debug logging

--- src/main/kotlin/sokos/ske/krav/client/SkeClient.kt
+++ src/main/kotlin/sokos/ske/krav/client/SkeClient.kt
@@ -1,23 +1,19 @@
 package sokos.ske.krav.client
 
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.engine.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.util.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToJsonElement
 import sokos.ske.krav.maskinporten.MaskinportenAccessTokenClient
 import sokos.ske.krav.skemodels.requests.AvskrivingRequest
 import sokos.ske.krav.skemodels.requests.EndringRequest
 import sokos.ske.krav.skemodels.requests.OpprettInnkrevingsoppdragRequest
-import sokos.ske.krav.skemodels.responses.OpprettInnkrevingsOppdragResponse
 
 private const val OPPRETT_KRAV = "innkrevingsoppdrag"
 private const val ENDRE_KRAV = "innkrevingsoppdrag/endring"
@@ -43,11 +39,11 @@ class SkeClient(
     private inline fun <reified T> toJson(serializer: SerializationStrategy<T>, body: T) = builder.encodeToJsonElement(serializer, body).toString()
 
     suspend fun opprettKrav(body: OpprettInnkrevingsoppdragRequest): HttpResponse = doPost(OPPRETT_KRAV, toJson(OpprettInnkrevingsoppdragRequest.serializer(), body))
-    suspend fun endreKrav(body: EndringRequest): HttpResponse = doPost(ENDRE_KRAV, toJson(EndringRequest.serializer(), body))
+    suspend fun endreKrav(body: EndringRequest): HttpResponse = doPut(ENDRE_KRAV, toJson(EndringRequest.serializer(), body))
     suspend fun stoppKrav(body: AvskrivingRequest): HttpResponse = doPost(STOPP_KRAV, toJson(AvskrivingRequest.serializer(), body))
     private suspend inline fun doPost(path: String, body: String): HttpResponse {
         val token = tokenProvider.hentAccessToken()
-        println(body)
+        println("doPost: $body")
         val response = client.post("$skeEndpoint$path") {
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
@@ -61,36 +57,9 @@ class SkeClient(
         return response
     }
 
-/*
-    suspend fun opprettKrav(body: OpprettInnkrevingsoppdragRequest): HttpResponse = doPost(OPPRETT_KRAV, body)
-    suspend fun endreKrav(body: EndringRequest): HttpResponse = doPost(ENDRE_KRAV, body)
-    suspend fun stoppKrav(body: AvskrivingRequest):HttpResponse = doPost(STOPP_KRAV, body)
-    private suspend inline fun <reified T> doPost(path: String, body: T): HttpResponse {
-        val token = tokenProvider.hentAccessToken()
-
-        val response = client.post("$skeEndpoint$path") {
-            contentType(ContentType.Application.Json)
-            accept(ContentType.Application.Json)
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-                append("Klientid", KLIENT_ID)
-            }
-            setBody(body)
-        }
-        println(response)
-        println("resp_body: ${response.bodyAsText()}, \n${response.headers}, \n${response.request.call}")
-        return response
-    }
-
-*/
-
-
-
-
-    @OptIn(InternalAPI::class)
     private suspend fun doPut(path: String, body: String): HttpResponse {
         val token = tokenProvider.hentAccessToken()
-
+        println("doPut: $body")
         val response = client.put("$skeEndpoint$path") {
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
@@ -101,10 +70,29 @@ class SkeClient(
             setBody(body)
         }
 
-        println("resp_body: ${response.bodyAsText()}, \n${response.content}, \n${response.request.call}")
+        println("resp_body: ${response.bodyAsText()}, \n${response.request.call}")
 
         return response
     }
+
+    private suspend fun doGet(path: String, body: String): HttpResponse {
+        val token = tokenProvider.hentAccessToken()
+        println("doGet: $body")
+        val response = client.get("$skeEndpoint$path") {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            headers {
+                append(HttpHeaders.Authorization, "Bearer $token")
+                append("Klientid", KLIENT_ID)
+            }
+            setBody(body)
+        }
+
+        println("resp_body: ${response.bodyAsText()}, \n${response.request.call}")
+
+        return response
+    }
+
 
 }
 
