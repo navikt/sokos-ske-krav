6bf3d7c933e3068623333d8022ff3bd5152916dd
KenDahle <ken.dahle@nav.no>
2023-09-25 19:28:53 +0200

    TOB-3018  db config

--- src/main/kotlin/sokos/ske/krav/database/PostgresDataSource.kt
+++ src/main/kotlin/sokos/ske/krav/database/PostgresDataSource.kt
@@ -36,14 +36,21 @@ class PostgresDataSource(private val postgresConfig: PropertiesConfig.PostgresCo
         else createHikariDataSourceWithVaultIntegration(hikariConfig(), postgresConfig.vaultMountPath, role)
 
     private fun hikariConfig() = HikariConfig().apply {
-        maximumPoolSize = 1000
+        minimumIdle = 1
+        maxLifetime = 26000
+        maximumPoolSize = 4
+        connectionTimeout = 250
         isAutoCommit = true
+        idleTimeout = 10001
+        isAutoCommit = false
         //connectionTestQuery = "SELECT * FROM ${dbConfig.testTable} LIMIT 1"
         jdbcUrl = postgresConfig.jdbcUrl
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         if (isLocal()) {
             username = postgresConfig.username
             password = postgresConfig.password
         }
     }
+
     private fun isLocal() = postgresConfig.vaultMountPath.isBlank()
 }
