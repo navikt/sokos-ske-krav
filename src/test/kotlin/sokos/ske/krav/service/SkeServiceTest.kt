2baba061b5e58623e121af291d028701ff0ee313
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2023-10-02 08:35:19 +0200

    Hva om jeg stopper ftpservice etter test...

--- src/test/kotlin/sokos/ske/krav/service/SkeServiceTest.kt
+++ src/test/kotlin/sokos/ske/krav/service/SkeServiceTest.kt
@@ -1,7 +1,6 @@
 package sokos.ske.krav.service
 
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.collections.shouldNotBeIn
@@ -16,12 +15,12 @@ import sokos.ske.krav.client.SkeClient
 import sokos.ske.krav.maskinporten.MaskinportenAccessTokenClient
 
 
-@Ignored
 internal class SkeServiceTest: FunSpec ({
 
     val tokenProvider = mockk<MaskinportenAccessTokenClient>(relaxed = true)
 
     test("Test OK filer"){
+        val ftpService = FtpService().apply {connect(fileNames = listOf("fil1.txt")) }
         val mockEngineOK = MockEngine {
             respond(
                 content = ByteReadChannel("{\"kravidentifikator\": \"1234\"}"),
@@ -34,13 +33,15 @@ internal class SkeServiceTest: FunSpec ({
             install(Logging){ level = LogLevel.INFO}
         }
         val client = SkeClient(skeEndpoint = "", client = httpClient, tokenProvider = tokenProvider)
-        val service = SkeService(client, FtpService().apply {connect(fileNames = listOf("fil1.txt")) })
+        val service = SkeService(client, ftpService)
         val responses = service.sendNyeFtpFilerTilSkatt()
         responses.map { it.status shouldBeIn listOf(HttpStatusCode.OK, HttpStatusCode.Created) }
+        ftpService.close()
         httpClient.close()
     }
 
     test("Test feilede filer"){
+        val ftpService =  FtpService().apply {connect(fileNames = listOf("fil1.txt")) }
         val mockEngineFail = MockEngine {
             respond(
                 content = ByteReadChannel("Feil i request"),
@@ -53,10 +54,11 @@ internal class SkeServiceTest: FunSpec ({
             install(Logging){ level = LogLevel.INFO}
         }
         val client = SkeClient(skeEndpoint = "", client = httpClient, tokenProvider = tokenProvider)
-        val service = SkeService(client, FtpService().apply {connect(fileNames = listOf("fil1.txt")) })
+        val service = SkeService(client, ftpService)
         val responses = service.sendNyeFtpFilerTilSkatt()
         responses.map { it.status shouldNotBeIn listOf(HttpStatusCode.OK, HttpStatusCode.Created) }
 
+        ftpService.close()
         httpClient.close()
     }
 })
