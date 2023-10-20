62d1d969df25a34c3518619489395ef2c4834ef0
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2023-10-20 09:54:18 +0200

    TOB-3018 La inn content negotiation

--- src/main/kotlin/sokos/ske/krav/util/HttpClient.kt
+++ src/main/kotlin/sokos/ske/krav/util/HttpClient.kt
@@ -1,8 +1,13 @@
 package sokos.ske.krav.util
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.jackson.jackson
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.net.ProxySelector
 
@@ -13,6 +18,17 @@ val httpClient = HttpClient(Apache) {
     install(HttpRequestRetry) {
         retryOnException(maxRetries = 3)
         delayMillis { retry -> retry * 3000L }
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            findAndRegisterModules()
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            enable(SerializationFeature.INDENT_OUTPUT)
+            setSerializationInclusion(JsonInclude.Include.NON_NULL)
+
+        }
     }
 
     engine {
