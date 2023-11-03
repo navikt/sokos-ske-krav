135320e921e687dd080e40b42f685295013851a2
KenDahle <ken.dahle@nav.no>
2023-11-03 12:28:43 +0100

    TOB-3018  isready isAlive er snudd

--- src/main/kotlin/sokos/ske/krav/api/NaisApi.kt
+++ src/main/kotlin/sokos/ske/krav/api/NaisApi.kt
@@ -1,17 +1,15 @@
 package sokos.ske.krav.api
 
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.call
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.Routing
-import io.ktor.server.routing.get
-import io.ktor.server.routing.route
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Routing.naisApi(alive: () -> Boolean, ready: () -> Boolean) {
 	route("internal") {
 		get("is_alive") {
 			when (alive()) {
-				true -> call.respondText { "Application is alive" }
+				false -> call.respondText { "Application is alive" }
 				else -> call.respondText(
 					text = "Application is not alive",
 					status = HttpStatusCode.InternalServerError
@@ -20,7 +18,7 @@ fun Routing.naisApi(alive: () -> Boolean, ready: () -> Boolean) {
 		}
 		get("is_ready") {
 			when (ready()) {
-				true -> call.respondText { "Application is ready" }
+				false -> call.respondText { "Application is ready" }
 				else -> call.respondText(
 					text = "Application is not ready",
 					status = HttpStatusCode.InternalServerError
