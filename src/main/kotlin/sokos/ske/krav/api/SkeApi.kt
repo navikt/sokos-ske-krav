b366ad8aab479d3ee9ebc843d87ea289c68164ed
KenDahle <ken.dahle@nav.no>
2023-11-07 11:49:54 +0100

    TOB-3018  Alerts

--- src/main/kotlin/sokos/ske/krav/api/SkeApi.kt
+++ src/main/kotlin/sokos/ske/krav/api/SkeApi.kt
@@ -42,7 +42,14 @@ fun Routing.skeApi(
 
 		get("error") {
 			(0..1000).forEach{
-				logger.error { "Nå er'e feil igjen $it av 1000" }
+				logger.error { "Nå er'e feil igjen, Error: $it" }
+			}
+			call.respond("Nå er det 1000 errors i loggen")
+		}
+
+		get("warn") {
+			(0..1000).forEach{
+				logger.warn { "Nå er'e feil igjen, Warning: $it" }
 			}
 			call.respond("Nå er det 1000 errors i loggen")
 		}
