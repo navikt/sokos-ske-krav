6de9563b3ea581317c7438f20cac81d0c016b59d
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2023-11-09 14:41:31 +0100

    TOB-3018 oppdaterte dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,139 +2,140 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.serialization") version "1.8.21"
-	id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.serialization") version "1.8.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 
 group = "no.nav.sokos"
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
-val ktorVersion = "2.3.5"
-val hikaricpVersion = "5.0.1"
-val jschVersion = "0.2.11"
-val nimbusVersion = "9.25.6"
+val ktorVersion = "2.3.6"
+val hikaricpVersion = "5.1.0"
+val jschVersion = "0.2.12"
+val nimbusVersion = "9.37.1"
 val postgresqlVersion = "42.6.0"
-val flywayVersion = "9.16.1"
+val flywayVersion = "10.0.0"
 val vaultVersion = "1.3.10"
 val natpryceVersion = "1.6.10.0"
-val kotlinxSerializationVersion = "1.5.1"
-val kotlinxDatetimeVersion = "0.4.0"
-val prometheusVersion = "1.11.3"
+val kotlinxSerializationVersion = "1.6.0"
+val kotlinxDatetimeVersion = "0.4.1"
+val prometheusVersion = "1.11.5"
 //Test
-val kotestVersion = "5.7.2"
-val mockkVersion = "1.13.7"
-val commonsVersion = "3.9.0"
+val kotestVersion = "5.8.0"
+val kotestTestContainerExtensionVersion = "2.0.2"
+val mockkVersion = "1.13.8"
+val commonsVersion = "3.10.0"
 val testContainerVersion = "1.19.1"
 val mockFtpServerVersion = "3.1.0"
 
 //Logging
 val janinoVersion = "3.1.10"
-val kotlinLoggingVersion = "3.0.4"
-val logbackVersion = "1.4.1"
-val logstashVersion = "7.3"
+val kotlinLoggingVersion = "3.0.5"
+val logbackVersion = "1.4.11"
+val logstashVersion = "7.4"
 
 dependencies {
-	// Ktor Server
-	implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
-	implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
-	implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-	implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    // Ktor Server
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
 
-	// Ktor Client
-	implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktorVersion")
-	implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
-	implementation("io.ktor:ktor-client-apache-jvm:$ktorVersion")
+    // Ktor Client
+    implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache-jvm:$ktorVersion")
 
-	// Security
-	implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
-	implementation("com.nimbusds:nimbus-jose-jwt:$nimbusVersion")
+    // Security
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
+    implementation("com.nimbusds:nimbus-jose-jwt:$nimbusVersion")
 
-	// Database
-	implementation("com.zaxxer:HikariCP:$hikaricpVersion")
-	implementation("org.postgresql:postgresql:$postgresqlVersion")
-	implementation("no.nav:vault-jdbc:$vaultVersion")
+    // Database
+    implementation("com.zaxxer:HikariCP:$hikaricpVersion")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    implementation("no.nav:vault-jdbc:$vaultVersion")
 
-	// Serialization
-	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinxSerializationVersion")
-	implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:$kotlinxDatetimeVersion")
+    // Serialization
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinxSerializationVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:$kotlinxDatetimeVersion")
 
-	// FTP
-	implementation("com.github.mwiede:jsch:$jschVersion")
+    // FTP
+    implementation("com.github.mwiede:jsch:$jschVersion")
 
-	// Config
-	implementation("com.natpryce:konfig:$natpryceVersion")
+    // Config
+    implementation("com.natpryce:konfig:$natpryceVersion")
 
-	// Flyway
-	implementation("org.flywaydb:flyway-core:$flywayVersion")
+    // Flyway
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
 
-	//metrics
-	implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
-	implementation("io.micrometer:micrometer-registry-prometheus:$prometheusVersion")
+    //metrics
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometheusVersion")
 
-	// Logging
-	implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
-	runtimeOnly("org.codehaus.janino:janino:$janinoVersion")
-	implementation("ch.qos.logback:logback-classic:$logbackVersion")
-	implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    // Logging
+    implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
+    runtimeOnly("org.codehaus.janino:janino:$janinoVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
 
-	// Test
-	testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
-	testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
-	testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
-	testImplementation("io.mockk:mockk:$mockkVersion")
-	testImplementation("org.testcontainers:postgresql:$testContainerVersion")
-	testImplementation("commons-net:commons-net:$commonsVersion")
-	testImplementation("org.mockftpserver:MockFtpServer:$mockFtpServerVersion")
+    // Test
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:$kotestTestContainerExtensionVersion")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
+    testImplementation("io.mockk:mockk-jvm:$mockkVersion")
+    testImplementation("org.testcontainers:postgresql:$testContainerVersion")
+    testImplementation("commons-net:commons-net:$commonsVersion")
+    testImplementation("org.mockftpserver:MockFtpServer:$mockFtpServerVersion")
 
 }
 
 kotlin {
-	jvmToolchain {
-		languageVersion.set(JavaLanguageVersion.of(17))
-	}
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 sourceSets {
-	main {
-		java {
-			srcDirs("${layout.buildDirectory.get()}/generated/src/main/kotlin")
-		}
-	}
+    main {
+        java {
+            srcDirs("${layout.buildDirectory.get()}/generated/src/main/kotlin")
+        }
+    }
 }
 
 
 tasks {
-	withType<ShadowJar>().configureEach {
-		enabled = true
-		archiveFileName.set("sokos-ske-krav.jar")
-		manifest {
-			attributes["Main-Class"] = "sokos.ske.krav.ApplicationKt"
-		}
-	}
+    withType<ShadowJar>().configureEach {
+        enabled = true
+        archiveFileName.set("sokos-ske-krav.jar")
+        manifest {
+            attributes["Main-Class"] = "sokos.ske.krav.ApplicationKt"
+        }
+    }
 
-	("jar") {
-		enabled = false
-	}
+    ("jar") {
+        enabled = false
+    }
 
 
-	withType<Test>().configureEach {
-		useJUnitPlatform()
-		testLogging {
-			exceptionFormat = TestExceptionFormat.FULL
-			events("passed", "skipped", "failed")
-		}
+    withType<Test>().configureEach {
+        useJUnitPlatform()
+        testLogging {
+            exceptionFormat = TestExceptionFormat.FULL
+            events("passed", "skipped", "failed")
+        }
 
-		reports.forEach { report -> report.required.value(false) }
-	}
+        reports.forEach { report -> report.required.value(false) }
+    }
 
-	withType<Wrapper> {
-		gradleVersion = "8.4"
-	}
+    withType<Wrapper> {
+        gradleVersion = "8.4"
+    }
 }
