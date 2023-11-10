591178ffa39e241cf390cd524e862d5d88524d0d
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2023-11-10 12:11:52 +0100

    TOB-3018 endret på responsestringer i MockHttpClient

--- src/test/kotlin/sokos/ske/krav/util/MockHttpClient.kt
+++ src/test/kotlin/sokos/ske/krav/util/MockHttpClient.kt
@@ -9,23 +9,25 @@ import io.ktor.http.headersOf
 import sokos.ske.krav.domain.ske.responses.MottaksStatusResponse
 
 class MockHttpClient(kravident: String = "1234", val iderForValideringsFeil: List<String> = listOf("23", "54", "87")) {
-    private val opprettResponse = "{\"kravidentifikator\": \"$kravident\"}"
+    private val opprettResponse = """{"kravidentifikator": "$kravident"}"""
 
-    private val mottattResponse = "{\n" +
-            "  \"kravidentifikator\": \"$kravident\",\n" +
-            "  \"oppdragsgiversKravidentifikator\": \"$kravident\",\n" +
-            "  \"mottaksstatus\": \"${MottaksStatusResponse.MottaksStatus.RESKONTROFOERT.value}\",\n" +
-            "  \"statusOppdatert\": \"2023-10-04T04:47:08.482Z\"\n" +
-            "}"
+    private val mottattResponse =
+        """{
+        |"kravidentifikator": "$kravident"
+        |"oppdragsgiversKravidentifikator": "$kravident"
+        |"mottaksstatus": "${MottaksStatusResponse.MottaksStatus.RESKONTROFOERT.value}"
+        |"statusOppdatert": "2023-10-04T04:47:08.482Z"
+        }
+        """.trimMargin()
 
-    private val valideringsfeilResponse = "{\n" +
-            "  \"valideringsfeil\": [\n" +
-            "    {\n" +
-            "      \"error\": \"feil\",\n" +
-            "      \"message\": \"melding\"\n" +
-            "    }\n" +
-            "  ]\n" +
-            "}"
+    private val valideringsfeilResponse =
+        """{
+        |"valideringsfeil": [{
+            |"error": "feil",
+            |"message": "melding"
+            }]
+        }
+        """.trimMargin()
 
     private val responseHeaders = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
     fun getClient(statusCode: HttpStatusCode = HttpStatusCode.OK) = HttpClient(MockEngine) {
@@ -36,7 +38,7 @@ class MockHttpClient(kravident: String = "1234", val iderForValideringsFeil: Lis
                         respond(mottattResponse, statusCode, responseHeaders)
                     }
 
-                    "/innkrevingsoppdrag//mottaksstatus" -> { //fordi stopp ikke er implementert
+                    "/innkrevingsoppdrag//mottaksstatus" -> { // fordi stopp ikke er implementert
                         respond(mottattResponse, statusCode, responseHeaders)
                     }
 
