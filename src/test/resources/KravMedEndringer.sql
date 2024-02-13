e43b4e1267a11dac13c887e797ad69f079eafaac
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2024-02-13 15:30:38 +0100

    TOB-3018 endrer testcontainer script...
    Co-authored-by: Ken Dahle <ken.dahle@nav.no>

--- src/test/resources/KravMedEndringer.sql
+++ src/test/resources/KravMedEndringer.sql
@@ -1,6 +1,6 @@
 INSERT INTO krav (kravidentifikator_ske, saksnummer_nav, belop, vedtakDato, gjelderId, periodeFOM, periodeTOM, kravkode, referanseNummerGammelSak, transaksjonDato, enhetBosted, enhetBehandlende, kodeHjemmel, kodeArsak, belopRente, fremtidigYtelse, utbetalDato, fagsystemId, status, kravtype, corr_id, dato_sendt, dato_siste_status)
-VALUES  ( '1234','OB040000573209', '123.00', '2023-05-24 00:00', '12345678901', '20220901', '20221031', 'PE AP','4321', '20230524', '4803', '4819','T','', '0.0', '0.0', '', '', 'MOTTATT_UNDER_BEHANDLING', 'ENDRE_RENTER', 'CORR!@#', '2023-01-01 12:00:00', '2023-01-01 13:00:00'),
-        ( '1234','OB040000598553', '456.00', '2023-05-25 00:00', '12345678902', '20221101', '20221231', 'PE AP','4321', '20230525', '4803', '4819','T','', '0.0', '0.0', '', '', 'MOTTATT_UNDER_BEHANDLING', 'ENDRE_HOVEDSTOL', 'CORR!2#', '2023-02-01 12:00:00', '2023-02-01 13:00:00');
+VALUES  ( '1234','OB040000573209', '123.00', '2023-05-24 00:00', '12345678901', '20220901', '20221031', 'PE AP','4321', '20230524', '4803', '4819','T','', '0.0', '0.0', '', '', 'MOTTATT_UNDER_BEHANDLING', 'ENDRE_RENTER', 'CORR123', '2023-01-01 12:00:00', '2023-01-01 13:00:00'),
+        ( '1234','OB040000598553', '456.00', '2023-05-25 00:00', '12345678902', '20221101', '20221231', 'PE AP','4321', '20230525', '4803', '4819','T','', '0.0', '0.0', '', '', 'MOTTATT_UNDER_BEHANDLING', 'ENDRE_HOVEDSTOL', 'CORR321', '2023-02-01 12:00:00', '2023-02-01 13:00:00');
 
 
 INSERT INTO kobling (saksref_fil, saksref_uuid, dato)
