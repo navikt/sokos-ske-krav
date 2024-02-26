2cb13c25b677db4b4ff76fac880b841bd4f2a7f3
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2024-02-26 13:35:51 +0100

    TOB-3018 endret sånn at man kan sette status for hver enkelt kall i MockHttpClient

--- src/test/kotlin/sokos/ske/krav/service/SkeServiceTest.kt
+++ src/test/kotlin/sokos/ske/krav/service/SkeServiceTest.kt
@@ -4,8 +4,8 @@ import com.zaxxer.hikari.HikariDataSource
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.testcontainers.toDataSource
 import io.kotest.matchers.shouldBe
-import io.ktor.client.*
-import io.ktor.http.*
+import io.ktor.client.HttpClient
+import io.ktor.http.HttpStatusCode
 import io.mockk.mockk
 import kotlinx.serialization.json.Json
 import sokos.ske.krav.client.SkeClient
@@ -24,7 +24,6 @@ import sokos.ske.krav.util.MockHttpClientUtils.Responses
 import sokos.ske.krav.util.MockHttpClientUtils.generateUrls
 import sokos.ske.krav.util.TestContainer
 
-
 internal class SkeServiceTest : FunSpec({
     fun startContainer(containerName: String, initScripts: List<String>): HikariDataSource {
         return TestContainer(containerName)
@@ -34,15 +33,9 @@ internal class SkeServiceTest : FunSpec({
                 minimumIdle = 4
                 isAutoCommit = false
             }
-
-
     }
 
-    fun setUpMockHttpClient(
-        endepunktTyper: List<MockRequestObj>,
-        statusCode: HttpStatusCode,
-    ) = MockHttpClient().getClient(endepunktTyper, statusCode)
-
+    fun setUpMockHttpClient(endepunktTyper: List<MockRequestObj>) = MockHttpClient().getClient(endepunktTyper)
 
     fun setupMocks(
         ftpFiler: List<String>,
@@ -63,25 +56,42 @@ internal class SkeServiceTest : FunSpec({
         val alarmService = AlarmService()
         val stoppKravService = StoppKravService(skeClient, databaseService)
 
-        return Pair(SkeService(skeClient, stoppKravService, endreKravService, opprettKravService, alarmService, databaseService, ftpService), dataSource)
+        return Pair(
+            SkeService(
+                skeClient,
+                stoppKravService,
+                endreKravService,
+                opprettKravService,
+                alarmService,
+                databaseService,
+                ftpService,
+            ),
+            dataSource,
+        )
     }
-
 
     test("Kravdata skal lagres i database etter å ha sendt nye krav til SKE") {
 
         println("Oppretter mockobjekter")
-        val nyttKravKall        = MockRequestObj(Responses.nyttKravResponse("1234"), listOf(EndepunktType.OPPRETT.url))
-        val avskrivKravKall     = MockRequestObj("", generateUrls(EndepunktType.AVSKRIVING.url))
-        val endreRenterKall     = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url))
-        val endreHovedstolKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url))
-        val endreReferanseKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url))
+        val nyttKravKall        = MockRequestObj(Responses.nyttKravResponse("1234"), listOf(EndepunktType.OPPRETT.url), HttpStatusCode.OK)
+        val avskrivKravKall     = MockRequestObj("", generateUrls(EndepunktType.AVSKRIVING.url), HttpStatusCode.OK)
+        val endreRenterKall     = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url), HttpStatusCode.OK)
+        val endreHovedstolKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url), HttpStatusCode.OK)
+        val endreReferanseKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url), HttpStatusCode.OK)
 
-        val httpClient = setUpMockHttpClient(listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall), HttpStatusCode.OK)
-        val mocks: Pair<SkeService, HikariDataSource> = setupMocks(listOf("FilMedBare10Linjer.txt"), this.testCase.name.testName, httpClient)
+        val httpClient =
+            setUpMockHttpClient(
+                listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall)
+            )
+        val mocks: Pair<SkeService, HikariDataSource> =
+            setupMocks(listOf("FilMedBare10Linjer.txt"), this.testCase.name.testName, httpClient)
 
         mocks.first.sendNewFilesToSKE()
 
-        val rs = mocks.second.connection.prepareStatement("""select count(*) as a from krav""".trimIndent()).executeQuery()
+        val rs =
+            mocks.second.connection.prepareStatement(
+                """select count(*) as a from krav""".trimIndent(),
+            ).executeQuery()
         rs.next()
         val kravdata = rs.getInt("a")
 
@@ -89,13 +99,13 @@ internal class SkeServiceTest : FunSpec({
     }
 
     test("Kravdata skal lagres med type som beskriver hva slags krav det er") {
-        val nyttKravKall        = MockRequestObj(Responses.nyttKravResponse("1234"), listOf(EndepunktType.OPPRETT.url))
-        val avskrivKravKall     = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.AVSKRIVING.url))
-        val endreRenterKall     = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url))
-        val endreHovedstolKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url))
-        val endreReferanseKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url))
+        val nyttKravKall        = MockRequestObj(Responses.nyttKravResponse("1234"), listOf(EndepunktType.OPPRETT.url), HttpStatusCode.OK)
+        val avskrivKravKall     = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.AVSKRIVING.url), HttpStatusCode.OK)
+        val endreRenterKall     = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url), HttpStatusCode.OK)
+        val endreHovedstolKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url), HttpStatusCode.OK)
+        val endreReferanseKall  = MockRequestObj(Responses.endringResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url), HttpStatusCode.OK)
 
-        val httpClient = setUpMockHttpClient(listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall), HttpStatusCode.OK)
+        val httpClient = setUpMockHttpClient(listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall))
         val mocks: Pair<SkeService, HikariDataSource> = setupMocks(listOf("AltOkFil.txt"), this.testCase.name.testName, httpClient)
 
         mocks.first.sendNewFilesToSKE()
@@ -104,7 +114,10 @@ internal class SkeServiceTest : FunSpec({
 
         println("Kravsett størrelse: ${kravSet.size}")
 
-        val endringsMap =  kravdata.filter{ it.kravtype == ENDRE_RENTER || it.kravtype == ENDRE_HOVEDSTOL}.toSet().groupBy { it.saksnummerSKE+it.saksnummerNAV }
+        val endringsMap =
+            kravdata.filter {
+                it.kravtype == ENDRE_RENTER || it.kravtype == ENDRE_HOVEDSTOL
+            }.toSet().groupBy { it.saksnummerSKE + it.saksnummerNAV }
 
         endringsMap.forEach {
             println("${it.key} : ${it.value.size}")
@@ -118,26 +131,31 @@ internal class SkeServiceTest : FunSpec({
     }
 
     test("Mottaksstatus skal oppdateres i database") {
-        val mottaksstatusKall = MockRequestObj(Responses.mottaksStatusResponse(status =  Status.RESKONTROFOERT.value), generateUrls(EndepunktType.MOTTAKSSTATUS.url))
+        val mottaksstatusKall = MockRequestObj(Responses.mottaksStatusResponse(status =  Status.RESKONTROFOERT.value), generateUrls(EndepunktType.MOTTAKSSTATUS.url), HttpStatusCode.OK)
 
-        val httpClient = setUpMockHttpClient(listOf(mottaksstatusKall), HttpStatusCode.OK)
-        val mocks: Pair<SkeService, HikariDataSource> = setupMocks(listOf("AltOkFil.txt"), this.testCase.name.testName, httpClient, listOf("NyeKrav.sql"))
+        val httpClient = setUpMockHttpClient(listOf(mottaksstatusKall))
+        val mocks: Pair<SkeService, HikariDataSource> =
+            setupMocks(listOf("AltOkFil.txt"), this.testCase.name.testName, httpClient, listOf("NyeKrav.sql"))
 
         mocks.first.hentOgOppdaterMottaksStatus()
         val kravdata = mocks.second.connection.getAllKrav()
-        
+
         kravdata.filter { it.status == Status.RESKONTROFOERT.value }.size shouldBe 2
     }
 
     test("Når et krav feiler skal det lagres i feilmeldingtabell") {
-        val nyttKravKall        = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), listOf(EndepunktType.OPPRETT.url))
-        val avskrivKravKall     = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.AVSKRIVING.url))
-        val endreRenterKall     = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url))
-        val endreHovedstolKall  = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url))
-        val endreReferanseKall  = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url))
+        val nyttKravKall        = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), listOf(EndepunktType.OPPRETT.url), HttpStatusCode.NotFound)
+        val avskrivKravKall     = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.AVSKRIVING.url), HttpStatusCode.NotFound)
+        val endreRenterKall     = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url), HttpStatusCode.NotFound)
+        val endreHovedstolKall  = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url), HttpStatusCode.NotFound)
+        val endreReferanseKall  = MockRequestObj(Responses.innkrevingsOppdragEksistererIkkeResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url), HttpStatusCode.NotFound)
 
-        val httpClient = setUpMockHttpClient(listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall), HttpStatusCode.NotFound)
-        val mocks: Pair<SkeService, HikariDataSource> = setupMocks(listOf("FilMedBare10Linjer.txt"),  this.testCase.name.testName, httpClient)
+        val httpClient =
+            setUpMockHttpClient(
+                listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall),
+            )
+        val mocks: Pair<SkeService, HikariDataSource> =
+            setupMocks(listOf("FilMedBare10Linjer.txt"), this.testCase.name.testName, httpClient)
 
         println("type: ${Json.decodeFromString<FeilResponse>(nyttKravKall.response).type}")
         mocks.first.sendNewFilesToSKE()
@@ -147,7 +165,10 @@ internal class SkeServiceTest : FunSpec({
         feilmeldinger.map { Json.decodeFromString<FeilResponse>(it.skeResponse).status == 404 }.size shouldBe 10
 
         val joinToString = feilmeldinger.joinToString("','") { it.corrId }.also(::println)
-        val kravMedFeil =  mocks.second.connection.prepareStatement("""select * from Krav where corr_id in ('$joinToString')""").executeQuery().toKrav()
+        val kravMedFeil =
+            mocks.second.connection.prepareStatement(
+                """select * from Krav where corr_id in ('$joinToString')""",
+            ).executeQuery().toKrav()
 
         kravMedFeil.size shouldBe 10
         kravMedFeil.filter { it.status == Status.FANT_IKKE_SAKSREF.value }.size shouldBe 10
@@ -155,31 +176,31 @@ internal class SkeServiceTest : FunSpec({
 
     test("Resending av krav ") {
 
-        val nyttKravKall        = MockRequestObj(Responses.innkrevingsOppdragEksistererFraFor(), listOf(EndepunktType.OPPRETT.url))
-        val avskrivKravKall     = MockRequestObj(Responses.innkrevingsOppdragHarUgyldigTilstandResponse(), generateUrls(EndepunktType.AVSKRIVING.url))
+        val nyttKravKall        = MockRequestObj(Responses.innkrevingsOppdragEksistererFraFor(), listOf(EndepunktType.OPPRETT.url), HttpStatusCode.UnprocessableEntity)
+        val avskrivKravKall     = MockRequestObj(Responses.innkrevingsOppdragHarUgyldigTilstandResponse(), generateUrls(EndepunktType.AVSKRIVING.url), HttpStatusCode.Conflict)
 
-        val endreRenterKall     = MockRequestObj(Responses.innkrevingsOppdragErIkkeReskontrofortResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url))
-        val endreHovedstolKall  = MockRequestObj(Responses.innkrevingsOppdragErIkkeReskontrofortResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url))
-        val endreReferanseKall  = MockRequestObj(Responses.innkrevingsOppdragErIkkeReskontrofortResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url))
+        val endreRenterKall     = MockRequestObj(Responses.innkrevingsOppdragErIkkeReskontrofortResponse(), generateUrls(EndepunktType.ENDRE_RENTER.url), HttpStatusCode.Conflict)
+        val endreHovedstolKall  = MockRequestObj(Responses.innkrevingsOppdragErIkkeReskontrofortResponse(), generateUrls(EndepunktType.ENDRE_HOVEDSTOL.url), HttpStatusCode.Conflict)
+        val endreReferanseKall  = MockRequestObj(Responses.innkrevingsOppdragErIkkeReskontrofortResponse(), generateUrls(EndepunktType.ENDRE_REFERANSE.url), HttpStatusCode.Conflict)
 
-        val httpClient = setUpMockHttpClient(listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall), HttpStatusCode.Conflict)
+        val httpClient = setUpMockHttpClient(listOf(nyttKravKall, avskrivKravKall, endreRenterKall, endreHovedstolKall, endreReferanseKall))
 
         val mocks: Pair<SkeService, HikariDataSource> = setupMocks(emptyList(), this.testCase.name.testName, httpClient, initScripts =  listOf("KravSomSkalResendes.sql"))
 
         val kravBefore = mocks.second.connection.prepareStatement("""select * from krav""").executeQuery().toKrav()
-        kravBefore.filter{it.status==Status.IKKE_RESKONTROFORT_RESEND.value}.size shouldBe 3
-        kravBefore.filter{it.status==Status.KRAV_IKKE_SENDT.value}.size shouldBe 1
-        kravBefore.filter{it.status==Status.KRAV_SENDT.value}.size shouldBe 1
+        kravBefore.filter { it.status == Status.IKKE_RESKONTROFORT_RESEND.value }.size shouldBe 3
+        kravBefore.filter { it.status == Status.KRAV_IKKE_SENDT.value }.size shouldBe 1
+        kravBefore.filter { it.status == Status.KRAV_SENDT.value }.size shouldBe 1
 
         mocks.first.resendIkkeReskontroforteKrav()
 
         val kravAfter = mocks.second.connection.prepareStatement("""select * from krav""").executeQuery().toKrav()
 
-        kravAfter.filter{it.status==Status.IKKE_RESKONTROFORT_RESEND.value}.size shouldBe 2
-        kravAfter.filter{it.status==Status.KRAV_IKKE_SENDT.value}.size shouldBe 0
-        kravAfter.filter{it.status==Status.ANNEN_KONFLIKT.value}.size shouldBe 2
-        kravAfter.filter{it.status==Status.KRAV_SENDT.value}.size shouldBe 1
-
+        kravAfter.forEach { println(it.status) }
+        kravAfter.filter { it.status == Status.IKKE_RESKONTROFORT_RESEND.value }.size shouldBe 2
+        kravAfter.filter { it.status == Status.KRAV_IKKE_SENDT.value }.size shouldBe 0
+        kravAfter.filter { it.status == Status.ANNEN_KONFLIKT.value }.size shouldBe 1
+        kravAfter.filter { it.status == Status.VALIDERINGSFEIL.value }.size shouldBe 1
+        kravAfter.filter { it.status == Status.KRAV_SENDT.value }.size shouldBe 1
     }
-
 })
--- src/test/kotlin/sokos/ske/krav/util/MockHttpClient.kt
+++ src/test/kotlin/sokos/ske/krav/util/MockHttpClient.kt
@@ -1,10 +1,13 @@
 package sokos.ske.krav.util
 
-import io.ktor.client.*
-import io.ktor.client.engine.mock.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
@@ -19,124 +22,132 @@ object MockHttpClientUtils {
 	ENDRE_REFERANSE("/oppdragsgiversreferanse"),
   }
 
-  data class MockRequestObj(
-	val response: String,
-	val urls: List<String>
-  )
+    data class MockRequestObj(
+        val response: String,
+        val urls: List<String>,
+        val statusCode: HttpStatusCode,
+    )
 
-  fun generateUrls(baseUrl: String) = mutableListOf(
-	"/innkrevingsoppdrag/1234$baseUrl",
-	"/innkrevingsoppdrag/OB040000592759$baseUrl",
-	"/innkrevingsoppdrag/OB040000479803$baseUrl",
-	"/innkrevingsoppdrag/OB040000595755$baseUrl",
-	"/innkrevingsoppdrag/1111-skeUUID$baseUrl",
-	"/innkrevingsoppdrag/2222-skeUUID$baseUrl",
-	"/innkrevingsoppdrag/$baseUrl",
-	"/innkrevingsoppdrag$baseUrl",
-	baseUrl
-  )
+    fun generateUrls(baseUrl: String) =
+        mutableListOf(
+            "/innkrevingsoppdrag/1234$baseUrl",
+            "/innkrevingsoppdrag/OB040000592759$baseUrl",
+            "/innkrevingsoppdrag/OB040000479803$baseUrl",
+            "/innkrevingsoppdrag/OB040000595755$baseUrl",
+            "/innkrevingsoppdrag/1111-skeUUID$baseUrl",
+            "/innkrevingsoppdrag/2222-skeUUID$baseUrl",
+            "/innkrevingsoppdrag/$baseUrl",
+            "/innkrevingsoppdrag$baseUrl",
+            baseUrl,
+        )
 
-  object Responses {
+    object Responses {
+        fun mottaksStatusResponse(
+            kravIdentifikator: String = "1234",
+            status: String,
+        ): String {
+            //language=json
+            return """
+                {
+                     "kravidentifikator": "$kravIdentifikator"
+                     "oppdragsgiversKravidentifikator": "4321"
+                     "mottaksstatus": "$status"
+                     "statusOppdatert": "2023-10-04T04:47:08.482Z"
+                     }
+                """.trimIndent()
+        }
 
-	fun mottaksStatusResponse(kravIdentifikator: String = "1234", status: String): String {
-	  //language=json
-	  return """
-       {
-            "kravidentifikator": "$kravIdentifikator"
-            "oppdragsgiversKravidentifikator": "4321"
-            "mottaksstatus": "$status"
-            "statusOppdatert": "2023-10-04T04:47:08.482Z"
+        fun nyttKravResponse(kravIdentifikator: String) = """{"kravidentifikator": "$kravIdentifikator"}"""
+
+        fun endringResponse(transaksjonsId: String = "791e5955-af86-42fe-b609-d4fc2754e35e") = """{"transaksjonsid": "$transaksjonsId"}"""
+
+        fun innkrevingsOppdragEksistererFraFor(kravIdentifikator: String = "1234") =
+            //language=json
+            """      
+            {
+                "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-eksisterer-fra-for",
+                "title":"Innkrevingsoppdrag eksisterer fra før",
+                "status":422,
+                "detail":"Innkrevingsoppdrag med oppdragsgiversKravidentifikator=$kravIdentifikator eksisterer fra før",
+                "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/avskriving"
             }
-        """.trimIndent()
-	}
+            """.trimIndent()
 
-	fun nyttKravResponse(kravIdentifikator: String) = """{"kravidentifikator": "$kravIdentifikator"}"""
-	fun endringResponse(transaksjonsId: String = "791e5955-af86-42fe-b609-d4fc2754e35e") = """{"transaksjonsid": "$transaksjonsId"}"""
+        fun innkrevingsOppdragEksistererIkkeResponse(kravIdentifikator: String = "1234") =
+            //language=json
+            """      
+            {
+                "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-eksisterer-ikke",
+                "title":"Innkrevingsoppdrag eksisterer ikke",
+                "status":404,
+                "detail":"Innkrevingsoppdrag med oppdragsgiversKravidentifikator=$kravIdentifikator eksisterer ikke",
+                "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/avskriving"
+            }
+            """.trimIndent()
 
+        //language=json
+        fun innkrevingsOppdragHarUgyldigTilstandResponse() =
+            """
+            {
+            "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-har-ugyldig-tilstand",
+            "title":"Innkrevingsoppdrag har ugyldig tilstand",
+            "status":409,
+            "detail":"Oppdrag med Kravidentifikatortype=SKATTEETATENS_KRAVIDENTIFIKATOR og Kravidentifikator=51dfc1fa-b241-4886-bb8f-a448e80c9b10 er ikke reskontroført og renter kan derfor ikke endres.",
+            "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/51dfc1fa-b241-4886-bb8f-a448e80c9b10/renter"
+            }
+            """.trimIndent()
 
-	fun innkrevingsOppdragEksistererFraFor(kravIdentifikator: String = "1234") =
-	  //language=json
-	  """      
-        {
-            "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-eksisterer-fra-for",
-            "title":"Innkrevingsoppdrag eksisterer fra før",
-            "status":422,
-            "detail":"Innkrevingsoppdrag med oppdragsgiversKravidentifikator=$kravIdentifikator eksisterer fra før",
-            "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/avskriving"
-        }
-        """.trimIndent()
+        //language=json
+        fun innkrevingsOppdragErIkkeReskontrofortResponse() =
+            """
+            {
+            "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-er-ikke-reskontrofoert",
+            "title":"Innkrevingsoppdraget er ikke reskontroført",
+            "status":409,
+            "detail":"Oppdrag med Kravidentifikatortype=SKATTEETATENS_KRAVIDENTIFIKATOR og Kravidentifikator=51dfc1fa-b241-4886-bb8f-a448e80c9b10 er ikke reskontroført og renter kan derfor ikke endres.",
+            "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/51dfc1fa-b241-4886-bb8f-a448e80c9b10/renter"
+            }
+            """.trimIndent()
 
-	  fun innkrevingsOppdragEksistererIkkeResponse(kravIdentifikator: String = "1234") =
-		  //language=json
-		  """      
-        {
-            "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-eksisterer-ikke",
-            "title":"Innkrevingsoppdrag eksisterer ikke",
-            "status":404,
-            "detail":"Innkrevingsoppdrag med oppdragsgiversKravidentifikator=$kravIdentifikator eksisterer ikke",
-            "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/avskriving"
-        }
-        """.trimIndent()
-
-
-
-	  //language=json
-	fun innkrevingsOppdragHarUgyldigTilstandResponse() =
-	  """
-        {
-        "type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-har-ugyldig-tilstand",
-        "title":"Innkrevingsoppdrag har ugyldig tilstand",
-        "status":409,
-        "detail":"Oppdrag med Kravidentifikatortype=SKATTEETATENS_KRAVIDENTIFIKATOR og Kravidentifikator=51dfc1fa-b241-4886-bb8f-a448e80c9b10 er ikke reskontroført og renter kan derfor ikke endres.",
-        "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/51dfc1fa-b241-4886-bb8f-a448e80c9b10/renter"
-        }
-    """.trimIndent()
-
-	  //language=json
-	  fun innkrevingsOppdragErIkkeReskontrofortResponse() =
-		  """
-        {
-		"type":"tag:skatteetaten.no,2024:innkreving:innkrevingsoppdrag:innkrevingsoppdrag-er-ikke-reskontrofoert",
-        "title":"Innkrevingsoppdraget er ikke reskontroført",
-        "status":409,
-        "detail":"Oppdrag med Kravidentifikatortype=SKATTEETATENS_KRAVIDENTIFIKATOR og Kravidentifikator=51dfc1fa-b241-4886-bb8f-a448e80c9b10 er ikke reskontroført og renter kan derfor ikke endres.",
-        "instance":"/api/innkreving/innkrevingsoppdrag/v1/innkrevingsoppdrag/51dfc1fa-b241-4886-bb8f-a448e80c9b10/renter"
-        }
-    """.trimIndent()
-
-
-	  //language=json
-	fun valideringsfeilResponse(error: String, message: String) =
-	  """{
-	  "valideringsfeil": [{
-		  "error":   "$error",
-		  "message": "$message"
-			}]
-		}
-        """.trimMargin()
-  }
+        //language=json
+        fun valideringsfeilResponse(
+            error: String,
+            message: String,
+        ) = """
+            {
+            "valideringsfeil": [{
+              "error":   "$error",
+              "message": "$message"
+            	}]
+            }
+            """.trimMargin()
+    }
 }
 
+class MockHttpClient {
+    private val responseHeaders = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+    private val jsonConfig =
+        Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+            encodeDefaults = true
+            @OptIn(ExperimentalSerializationApi::class)
+            explicitNulls = false
+        }
 
-class MockHttpClient() {
-  private val responseHeaders = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
-  private val jsonConfig = Json {
-	prettyPrint = true
-	ignoreUnknownKeys = true
-	encodeDefaults = true
-	@OptIn(ExperimentalSerializationApi::class)
-	explicitNulls = false
-  }
-
-
-  fun getClient(kall: List<MockHttpClientUtils.MockRequestObj>, statusCode: HttpStatusCode) = HttpClient(MockEngine) {
-	install(ContentNegotiation) { json(jsonConfig) }
-	engine {
-	  addHandler { request ->
-		val handler = kall.singleOrNull() { it.urls.contains(request.url.encodedPath) }
-		if (handler != null) respond(handler.response, statusCode, responseHeaders)
-		else error("Ikke implementert: ${request.url.encodedPath}")
-	  }
-	}
-  }
+    fun getClient(
+        kall: List<MockHttpClientUtils.MockRequestObj>,
+    ) = HttpClient(MockEngine) {
+        install(ContentNegotiation) { json(jsonConfig) }
+        engine {
+            addHandler { request ->
+                val handler = kall.singleOrNull { it.urls.contains(request.url.encodedPath) }
+                if (handler != null) {
+                    respond(handler.response, handler.statusCode, responseHeaders)
+                } else {
+                    error("Ikke implementert: ${request.url.encodedPath}")
+                }
+            }
+        }
+    }
 }
