2a3c99fbdb22843534b8f37204e430df90021078
KenDahle <ken.dahle@nav.no>
2024-03-04 16:25:07 +0100

    TOB-3018 lagt til noen valideringer

--- src/main/kotlin/sokos/ske/krav/validation/LineValidator.kt
+++ src/main/kotlin/sokos/ske/krav/validation/LineValidator.kt
@@ -4,6 +4,7 @@ import mu.KotlinLogging
 import sokos.ske.krav.domain.nav.KravLinje
 import sokos.ske.krav.domain.nav.KravtypeMappingFromNAVToSKE
 import sokos.ske.krav.metrics.Metrics
+import java.time.LocalDate
 
 private val logger = KotlinLogging.logger{}
 
@@ -32,4 +33,26 @@ object LineValidator{
         logger.info( "Feil i $filNavn på linje ${krav.linjeNummer}: ${e.message}")
         false
     }
+
+    //Må være mellom 1 og 40 tegn, og kun inneholde bokstaver (a-å, A-Å), tall og spesialtegnene - og /
+    private fun validateSaksnr(krav: KravLinje, filNavn: String): Boolean  {
+
+        return true
+    }
+
+    //KAn ikke være i fremtiden
+    private fun validateVedtaksdato(krav: KravLinje, filNavn: String): Boolean  {
+
+        return true
+    }
+
+    private fun validateDatoIFortid(date: LocalDate): Boolean  {
+        return !validateDatoIFremtid(date)
+    }
+
+    private fun validateDatoIFremtid(date: LocalDate) = date.isAfter(LocalDate.now())
+
+
+    private fun validateFomBeforetom(fom: LocalDate, tom: LocalDate) = fom.isBefore(tom)
+
 }
