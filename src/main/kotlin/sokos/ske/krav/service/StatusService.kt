71a2f8e23d90b8bb2dfda0956a7df0400cc0879f
KenDahle <ken.dahle@nav.no>
2024-03-14 14:55:37 +0100

    TOB-3018  Bugfix StatusService
    Co-authored-by: Thea Marie Alnæs <thea.marie.alnes@nav.no>

--- src/main/kotlin/sokos/ske/krav/service/StatusService.kt
+++ src/main/kotlin/sokos/ske/krav/service/StatusService.kt
@@ -92,10 +92,11 @@ class StatusService(
                 )
                 databaseService.saveFeilmelding(feilmeldingTable, kravTable.corr_id)
             }
+        } else {
+            logger.error { "Kall til henting av valideringsfeil hos SKE feilet: ${response.status.value}, ${response.status.description}" }
         }
-        logger.error { "Kall til henting av valideringsfeil hos SKE feilet: ${response.status.value}, ${response.status.description}" }
     }
 
-   fun hentValideringsfeil() = databaseService.getAllFeilmeldinger()
+    fun hentValideringsfeil() = databaseService.getAllFeilmeldinger()
 
 }
