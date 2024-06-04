12446f4f41f57716892ea1c228b8addacb52bbf4
KenDahle <ken.dahle@nav.no>
2024-06-04 23:41:49 +0200

    TOB-3018 fikset noen importer

--- src/main/kotlin/sokos/ske/krav/config/Configuration.kt
+++ src/main/kotlin/sokos/ske/krav/config/Configuration.kt
@@ -1,5 +1,17 @@
 package sokos.ske.krav.config
 
+import com.auth0.jwk.JwkProvider
+import com.auth0.jwk.JwkProviderBuilder
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import sokos.ske.krav.util.httpClient
+import java.net.URI
+import java.util.concurrent.TimeUnit
+
+private val logger = KotlinLogging.logger {  }
 data class Configuration(
     val appName:String = readProperty("NAIS_APP_NAME"),
     val azureAd: AzureAd = AzureAd(),
@@ -37,3 +49,9 @@ data class Configuration(
     )
 
 }
+
+private fun readProperty(name: String, default: String? = null) =
+    System.getenv(name)
+        ?: System.getProperty(name)
+        ?: default.takeIf { it != null }?.also { logger.warn { "Using default value for property $name" } }
+        ?: throw RuntimeException("Mandatory property '$name' was not found")
