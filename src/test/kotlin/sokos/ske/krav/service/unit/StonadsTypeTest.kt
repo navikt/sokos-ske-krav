d4d9e9af8581e5f68b37f4428cd6bf60482f177b
Thea Marie Alnes <thea.marie.alnaes@nav.no>
2025-02-25 14:21:02 +0100

    :white_check_mark: Fullført Stønadstypetest

--- src/test/kotlin/sokos/ske/krav/service/unit/StonadsTypeTest.kt
+++ src/test/kotlin/sokos/ske/krav/service/unit/StonadsTypeTest.kt
@@ -7,18 +7,46 @@ import io.mockk.every
 import io.mockk.mockk
 import sokos.ske.krav.database.models.KravTable
 import sokos.ske.krav.domain.StonadsType
-import sokos.ske.krav.domain.nav.KravLinje
 
 internal class StonadsTypeTest :
     FunSpec({
 
-        test("getStonadstype should return correct StonadsType for all KravTable combinations") {
+        test("getStonadstype skal returnere korrekt StonadsType for alle kombinasjoner") {
             val kravMap =
                 mapOf(
                     Pair("BA OR", "T") to StonadsType.TILBAKEKREVING_BARNETRYGD,
-            /*        Pair("EN OR", "E") to StonadsType.TILBAKEKREVING_ENSLIG_FORSORGER,
-                    Pair("SU PV", "S") to StonadsType.TILBAKEKREVING_SUPPLERENDE,
-                    Pair("AA OR", "A") to StonadsType.TILBAKEKREVING_ARBEIDSAVKLARING,*/
+                    Pair("BS OM", "T") to StonadsType.TILBAKEKREVING_OMSORGSPENGER,
+                    Pair("BS PN", "T") to StonadsType.TILBAKEKREVING_PLEIEPENGER_BARN,
+                    Pair("BS PP", "T") to StonadsType.TILBAKEKREVING_PLEIEPENGER_NAERSTAAENDE,
+                    Pair("EF BT", "T") to StonadsType.TILBAKEKREVING_STOENAD_TIL_BARNETILSYN,
+                    Pair("EF OG", "T") to StonadsType.TILBAKEKREVING_OVERGANGSSTOENAD,
+                    Pair("FA FE", "T") to StonadsType.TILBAKEKREVING_ENGANGSSTOENAD_VED_FOEDSEL,
+                    Pair("FA FØ", "T") to StonadsType.TILBAKEKREVING_FORELDREPENGER,
+                    Pair("FA SV", "T") to StonadsType.TILBAKEKREVING_SVANGERSKAPSPENGER,
+                    Pair("FO FT", "FT") to StonadsType.TILBAKEKREVING_FORSKUTTERTE_DAGPENGER,
+                    Pair("FR SN", "T") to StonadsType.TILBAKEKREVING_KOMPENSASJON_NAERING_OG_FRILANS,
+                    Pair("KT SP", "T") to StonadsType.TILBAKEKREVING_SYKEPENGER,
+                    Pair("LK LK", "T") to StonadsType.TILBAKEKREVING_PERMITTERINGSPENGER_KORONA,
+                    Pair("LK RF", "T") to StonadsType.TILBAKEKREVING_LOENNSKOMPENSASJON,
+                    Pair("PE AF", "T") to StonadsType.TILBAKEKREVING_AVTALEFESTET_PENSJON_PRIVATSEKTOR,
+                    Pair("PE AP", "T") to StonadsType.TILBAKEKREVING_ALDERSPENSJON,
+                    Pair("PE BP", "T") to StonadsType.TILBAKEKREVING_BARNEPENSJON,
+                    Pair("PE GP", "T") to StonadsType.TILBAKEKREVING_GJENLEVENDE_PENSJON,
+                    Pair("PE GP", "TA") to StonadsType.TILBAKEKREVING_GJENLEVENDE_PENSJON_AVREGNING,
+                    Pair("PE KP", "T") to StonadsType.TILBAKEKREVING_KRIGSPENSJON,
+                    Pair("PE UP", "T") to StonadsType.TILBAKEKREVING_UFOEREPENSJON,
+                    Pair("PE UT", "T") to StonadsType.TILBAKEKREVING_UFOERETRYGD,
+                    Pair("PE UT", "EU") to StonadsType.TILBAKEKREVING_UFOERETRYGD_ETTEROPPGJOER,
+                    Pair("PE UT", "TA") to StonadsType.TILBAKEKREVING_UFOERETRYGD_AVREGNING,
+                    Pair("PE XP", "T") to StonadsType.TILBAKEKREVING_AVTALEFESTET_PENSJON,
+                    Pair("SU AP", "T") to StonadsType.TILBAKEKREVING_SUPPLERENDE_STOENAD_ALDERSPENSJON,
+                    Pair("SU UF", "T") to StonadsType.TILBAKEKREVING_SUPPLERENDE_STOENAD_UFOEREPENSJON,
+                    Pair("BS OP", "T") to StonadsType.TILBAKEKREVING_OPPLAERINGSPENGER,
+                    Pair("EF UT", "T") to StonadsType.TILBAKEKREVING_UTDANNINGSSTOENAD,
+                    Pair("KS KS", "T") to StonadsType.TILBAKEKREVING_KONTANTSTOETTE,
+                    Pair("PE FP", "T") to StonadsType.TILBAKEKREVING_TIDLIGERE_FAMILIEPLEIER_PENSJON,
+                    Pair("PE GY", "T") to StonadsType.TILBAKEKREVING_GAMMEL_YRKESSKADEPENSJON,
+                    Pair("OM OM", "T") to StonadsType.TILBAKEKREVING_OMSTILLINGSSTOENAD,
                 )
 
             kravMap.forEach { (input, expected) ->
@@ -32,21 +60,10 @@ internal class StonadsTypeTest :
             }
         }
 
-        test("getStonadstype should throw NotImplementedError for unknown KravTable") {
+        test("getStonadstypes skal kaste NotImplementedError for ukjent kombinasjon") {
             val krav =
                 mockk<KravTable> {
                     every { kravkode } returns "UNKNOWN"
-                    every { kodeHjemmel } returns "UNKNOWN"
-                }
-            shouldThrow<NotImplementedError> {
-                StonadsType.getStonadstype(krav)
-            }
-        }
-
-        test("getStonadstype should throw NotImplementedError for unknown KravLinje") {
-            val krav =
-                mockk<KravLinje> {
-                    every { kravKode } returns "UNKNOWN"
                     every { kodeHjemmel } returns "UNKNOWN"
                 }
             shouldThrow<NotImplementedError> {
