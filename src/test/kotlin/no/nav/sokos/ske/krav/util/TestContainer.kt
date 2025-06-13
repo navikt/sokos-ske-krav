f06a8c6b6df2bdc54ac97b5b806010d6abf1a77c
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-06-13 14:26:28 +0200

    Test om det bygger på github med opprinnelig oppsett

--- src/test/kotlin/no/nav/sokos/ske/krav/util/TestContainer.kt
+++ src/test/kotlin/no/nav/sokos/ske/krav/util/TestContainer.kt
@@ -1,7 +1,5 @@
 package no.nav.sokos.ske.krav.util
 
-import java.time.Duration
-
 import com.zaxxer.hikari.HikariDataSource
 import io.kotest.extensions.testcontainers.toDataSource
 import org.testcontainers.containers.PostgreSQLContainer
@@ -13,8 +11,6 @@ import org.testcontainers.utility.DockerImageName
 import no.nav.sokos.ske.krav.config.PropertiesConfig
 import no.nav.sokos.ske.krav.database.PostgresDataSource
 
-private val logger = mu.KotlinLogging.logger {}
-
 class TestContainer {
     private val properties = PropertiesConfig.PostgresConfig
     private val dockerImageName = "postgres:latest"
@@ -22,28 +18,19 @@ class TestContainer {
         PostgreSQLContainer<Nothing>(DockerImageName.parse(dockerImageName)).apply {
             withReuse(false)
             withUsername(properties.adminUser)
-            waitingFor(
-                Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 2)
-                    .withStartupTimeout(Duration.ofSeconds(60)),
-            )
+            waitingFor(Wait.defaultWaitStrategy())
             start()
-            logger.info("PostgreSQL container started on port: ${getMappedPort(5432)}")
         }
 
-    val dataSource: HikariDataSource by lazy {
+    val dataSource: HikariDataSource =
         container.toDataSource {
             maximumPoolSize = 100
             minimumIdle = 1
             isAutoCommit = false
-            connectionTimeout = 30000
-            initializationFailTimeout = 10000
         }
-    }
 
     init {
-        require(container.isRunning) { "PostgreSQL container is not running" }
         PostgresDataSource.migrate(dataSource)
-        logger.info("PostgreSQL container migration completed")
     }
 
     fun migrate(script: String = "") {
