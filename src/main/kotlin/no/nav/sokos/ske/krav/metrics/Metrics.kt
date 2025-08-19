80fd5eac0e0c66ec0c354e92d72e906f43c29a8c
takwaiwang <tak.wai.wang@nav.no>
2025-08-19 13:05:13 +0200

    Fiks MeterFilter is being configured after a Meter has been registered to this registry

--- src/main/kotlin/no/nav/sokos/ske/krav/metrics/Metrics.kt
+++ src/main/kotlin/no/nav/sokos/ske/krav/metrics/Metrics.kt
@@ -10,13 +10,13 @@ object Metrics {
     val registry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
     private const val NAMESPACE = "sokos_ske_krav"
 
-    val numberOfKravRead: Counter = counter("krav_innlest_lest", "Antall krav lest fra fil")
-    val numberOfKravSent: Counter = counter("krav_sendt_til_ske", "Antall krav sendt til endepunkt")
-    val numberOfNyeKrav: Counter = counter("nye_krav_sendt", "Antall nye krav sendt")
-    val numberOfEndringerAvKrav: Counter = counter("endringer_krav_sendt", "Antall endringer av krav sendt")
-    val numberOfStoppAvKrav: Counter = counter("stopp_krav_sendt", "Antall stopp av krav sendt")
-    val numberOfKravFeilet: Counter = counter("krav_feilet_i_innsending", "Antall krav som har feilet")
-    val numberOfKravResent: Counter = counter("krav_resendt_til_ske", "Antall krav som er resendt")
+    val numberOfKravRead: Counter by lazy { counter("krav_innlest_lest", "Antall krav lest fra fil") }
+    val numberOfKravSent: Counter by lazy { counter("krav_sendt_til_ske", "Antall krav sendt til endepunkt") }
+    val numberOfNyeKrav: Counter by lazy { counter("nye_krav_sendt", "Antall nye krav sendt") }
+    val numberOfEndringerAvKrav: Counter by lazy { counter("endringer_krav_sendt", "Antall endringer av krav sendt") }
+    val numberOfStoppAvKrav: Counter by lazy { counter("stopp_krav_sendt", "Antall stopp av krav sendt") }
+    val numberOfKravFeilet: Counter by lazy { counter("krav_feilet_i_innsending", "Antall krav som har feilet") }
+    val numberOfKravResent: Counter by lazy { counter("krav_resendt_til_ske", "Antall krav som er resendt") }
 
     private val kodeKravSendtCounters = ConcurrentHashMap<String, Counter>()
 
