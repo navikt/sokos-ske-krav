c65909c2c037cbfc4e9cc789763752be30a8cffb
Thea Marie Alnes <thea.marie.alnes@nav.no>
2026-02-20 10:25:13 +0100

    TOB-6156 La inn logging for å debugge innsending

--- src/main/kotlin/no/nav/sokos/ske/krav/service/OpprettKravService.kt
+++ src/main/kotlin/no/nav/sokos/ske/krav/service/OpprettKravService.kt
@@ -1,16 +1,19 @@
 package no.nav.sokos.ske.krav.service
 
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.isSuccess
 
 import no.nav.sokos.ske.krav.client.SkeClient
 import no.nav.sokos.ske.krav.config.CircuitBreakerException
+import no.nav.sokos.ske.krav.config.jsonConfig
 import no.nav.sokos.ske.krav.domain.Krav
 import no.nav.sokos.ske.krav.dto.ske.responses.OpprettInnkrevingsOppdragResponse
 import no.nav.sokos.ske.krav.util.RequestResult
 import no.nav.sokos.ske.krav.util.createOpprettKravRequest
 import no.nav.sokos.ske.krav.util.defineStatus
 import no.nav.sokos.ske.krav.util.encodeToString
-import no.nav.sokos.ske.krav.util.parseTo
 
 class OpprettKravService(
     private val skeClient: SkeClient,
@@ -32,16 +35,31 @@ class OpprettKravService(
         return requestResults
     }
 
+    // Kun for å få tester til å passe
+    fun decodeToOpprettInnkrevingsOppdragResponse(response: String): OpprettInnkrevingsOppdragResponse = jsonConfig.decodeFromString<OpprettInnkrevingsOppdragResponse>(response)
+
     private suspend fun sendOpprettKrav(krav: Krav): RequestResult {
         val opprettKravRequest = createOpprettKravRequest(krav)
-        val response = skeClient.opprettKrav(opprettKravRequest, krav.corrId)
+        val response: HttpResponse = skeClient.opprettKrav(opprettKravRequest, krav.corrId)
+        val responseBody: String = response.bodyAsText()
+        val status = defineStatus(responseBody, response.status)
+        val kravidentifikator =
+            if (response.status.isSuccess()) {
+                try {
+                    decodeToOpprettInnkrevingsOppdragResponse(responseBody).kravidentifikator
+                } catch (e: Exception) {
+                    ""
+                }
+            } else {
+                ""
+            }
 
         return RequestResult(
             response = response,
             request = opprettKravRequest.encodeToString(),
             krav = krav,
-            kravidentifikator = response.parseTo<OpprettInnkrevingsOppdragResponse>()?.kravidentifikator ?: "",
-            status = defineStatus(response),
+            kravidentifikator = kravidentifikator,
+            status = status,
         )
     }
 }
