d920884facf0e19f3dd65d3c9de2f26af6d5b2bc
Thea Marie Alnes <thea.marie.alnes@nav.no>
2026-03-04 14:30:16 +0100

    TOB-6178 403 debugging

--- src/main/kotlin/no/nav/sokos/ske/krav/config/CircuitBreakerPlugin.kt
+++ src/main/kotlin/no/nav/sokos/ske/krav/config/CircuitBreakerPlugin.kt
@@ -17,7 +17,7 @@ val CircuitBreakerPlugin =
         onResponse { response ->
             if (response.isFailure()) {
                 logger.error {
-                    "Circuit breaker treating response as failure: ${response.status.value} for ${response.request.url} "
+                    "Circuit breaker treating response as failure: ${response.status.value} ${response.status.description} from ${response.request.url} "
                 }
                 throw CircuitBreakerException("HTTP ${response.status.value}: ${response.status.description} from ${response.request.url}")
             }
