45627fe4dfd01657c80051d95dcf7f04081d5d2f
laodice <laodice.melliti@gmail.com>
2026-04-13 11:57:54 +0200

    TOB-6379: Legge til eksplisitt timestamp i sql filene

--- src/test/resources/SQLscript/validering/FilValideringsFeil.sql
+++ src/test/resources/SQLscript/validering/FilValideringsFeil.sql
@@ -1,8 +1,8 @@
 insert into filvalideringsfeil (id, filnavn, linjenummer, saksnummer_nav, kravlinje, feilmelding, tidspunkt_opprettet)
 values
-    (11, 'Fil1.txt', 1, '111', 'linje1', 'feilmelding1', '2023-01-01 11:00:00'),
-    (21, 'Fil2.txt', 2, '222', 'linje2.1', 'feilmelding2.1', '2023-01-01 11:00:00'),
-    (22, 'Fil2.txt', 2, '222', 'linje2.2', 'feilmelding2.2', '2023-01-02 11:00:00'),
-    (31, 'Fil3.txt', 3, '333', 'linje3.1', 'feilmelding3.1', '2023-01-02 11:00:00'),
-    (32, 'Fil3.txt', 3, '333', 'linje3.2', 'feilmelding3.2', '2023-01-03 11:00:00'),
-    (33, 'Fil3.txt', 3, '333', 'linje3.3', 'feilmelding3.3', '2023-01-03 11:00:00');
+    (11, 'Fil1.txt', 1, '111', 'linje1', 'feilmelding1', TIMESTAMP '2023-01-01 11:00:00'),
+    (21, 'Fil2.txt', 2, '222', 'linje2.1', 'feilmelding2.1', TIMESTAMP '2023-01-01 11:00:00'),
+    (22, 'Fil2.txt', 2, '222', 'linje2.2', 'feilmelding2.2', TIMESTAMP '2023-01-02 11:00:00'),
+    (31, 'Fil3.txt', 3, '333', 'linje3.1', 'feilmelding3.1', TIMESTAMP '2023-01-02 11:00:00'),
+    (32, 'Fil3.txt', 3, '333', 'linje3.2', 'feilmelding3.2', TIMESTAMP '2023-01-03 11:00:00'),
+    (33, 'Fil3.txt', 3, '333', 'linje3.3', 'feilmelding3.3', TIMESTAMP '2023-01-03 11:00:00');
