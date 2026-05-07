d0ad925eeb61463c25b5d226bfddfde0f0adc473
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2026-05-07 13:17:12 +0200

    Aggregér logg per fil (#287)
    * Aggregér logg per fil
    
    Loggen med vellykkede kravoverføringer splittes i én linje per fil i kjøringen
    
    * Logg feilede ting aggregert per fil også
    
    * Erstatt Triple med egen dataklasse
    
    * Tell antall unsuccessfull, ikke bare skriv ut hele lista
    
    * Fiks compile-feil innført av endring fra Triple til Telling
    
    note to self: bygg riktig prosjekt
    
    * Legg til integrasjonstest av logg-aggregering
    
    Som side-effekt: Legg til logg-mocking i SkeServiceIntegrationTest.kt
    
    * Fjern en unødvendig runBlocking
    
    * Husk å unmockke KotlinLogging etter at vi er ferdig med den
    
    * Flytt Telling og aggregering inn i en intern companion til SkeService
    
    Dette gjør det tydligere at det ikke er en tjeneste vi nødvendigvis tilbyr (mvh Copilot). Velger likevel å holde de tilgjengelige, så vi kan drive enhetstesting mot de.
    
    * Oversetter Telling til engelsk

--- src/test/kotlin/no/nav/sokos/ske/krav/service/integration/SkeServiceIntegrationTest.kt
+++ src/test/kotlin/no/nav/sokos/ske/krav/service/integration/SkeServiceIntegrationTest.kt
@@ -7,8 +7,17 @@ import io.kotest.matchers.string.shouldContain
 import io.ktor.http.HttpStatusCode
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.runs
 import io.mockk.spyk
+import io.mockk.unmockkObject
+import io.mockk.verify
+import mu.KLogger
+import mu.KotlinLogging
+import mu.Marker
 
 import no.nav.sokos.ske.krav.client.SkeClient
 import no.nav.sokos.ske.krav.client.SlackClient
@@ -49,6 +58,21 @@ import no.nav.sokos.ske.krav.validation.FileValidator
 internal class SkeServiceIntegrationTest :
     BehaviorSpec({
         extensions(SftpListener, DBListener)
+        val logger =
+            mockk<KLogger>(relaxed = true) {
+                every { info(any<() -> Unit>()) } just runs
+                every { warn(any<() -> Unit>()) } just runs
+                every { error(any<() -> Unit>()) } just runs
+                every { error(any<String>()) } just runs
+                every { error(any<Marker>(), any<() -> Unit>()) } just runs
+            }
+        beforeSpec {
+            mockkObject(KotlinLogging)
+            every { KotlinLogging.logger(any<() -> Unit>()) } returns logger
+        }
+        afterSpec {
+            unmockkObject(KotlinLogging)
+        }
         beforeEach {
             circuitBreaker.reset()
         }
@@ -252,6 +276,25 @@ internal class SkeServiceIntegrationTest :
                     }
 
                 kravMedFeil.filter { it.status == Status.HTTP422_VALIDERINGSFEIL.value }.size shouldBe 10
+            }
+        }
+
+        Given("To filer med krav legges i INBOUND") {
+            DBListener.clearDB()
+            SftpListener.putFiles(listOf("krav/TiNyeKrav.txt", "krav/UtenFremtidigYtelse.txt"), Directories.INBOUND)
+
+            val nyttKravKall = MockResponse(Endpoint.OPPRETT, nyttKravResponse(), HttpStatusCode.OK)
+            val mottaksstatusKall = MockResponse(Endpoint.MOTTAKSSTATUS, mottaksStatusResponse(), HttpStatusCode.OK)
+            val httpClient = MockHttpClient.client(nyttKravKall, mottaksstatusKall)
+            val dbService = DatabaseService(DBListener.dataSource)
+            val skeService = setupSkeServiceMockWithMockEngine(DBListener.dataSource, httpClient, ftpService, dbService)
+
+            skeService.handleNewKrav()
+            Then("skal det logges rett antall krav per fil") {
+                verify(exactly = 1) {
+                    logger.info("Fil: TiNyeKrav.txt - Nye: 10, Endringer: 0, Stopp: 0")
+                    logger.info("Fil: UtenFremtidigYtelse.txt - Nye: 5, Endringer: 0, Stopp: 0")
+                }
             }
         }
 
