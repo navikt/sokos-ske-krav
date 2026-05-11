3a00b7fb3efe5e377598b2aecea0ab8c486e2936
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2026-05-11 13:28:00 +0200

    test med admin i hikariConfig og endre temp tabell

--- src/main/kotlin/no/nav/sokos/ske/krav/config/PostgresDataSource.kt
+++ src/main/kotlin/no/nav/sokos/ske/krav/config/PostgresDataSource.kt
@@ -19,7 +19,11 @@ object PostgresDataSource {
     }
 
     fun migrate() {
-        dataSource(role = postgresConfig.adminUser).use { migrate(it) }
+        val migrationConfig =
+            hikariConfig().apply {
+                connectionInitSql = """SET ROLE "${postgresConfig.adminUser}""""
+            }
+        dataSource(hikariConfig = migrationConfig, role = postgresConfig.adminUser).use { migrate(it) }
     }
 
     fun migrate(dataSource: HikariDataSource) {
---
+++ src/main/resources/db/migration/V1.0.9__alter_feilmelding_temp.sql
@@ -0,0 +1,1 @@
+ALTER TABLE feilmeldingtemp RENAME TO temp;
