7fc0da27aba3d5ee64fd71c33cd2e8386db6b3f4
laodice <laodice.melliti@gmail.com>
2026-05-20 14:20:39 +0200

    TOB-6309: Konverterte DBUtilsTest til å bruke ekte logger istedet av en mock

--- src/test/kotlin/no/nav/sokos/ske/krav/util/DBUtilsTest.kt
+++ src/test/kotlin/no/nav/sokos/ske/krav/util/DBUtilsTest.kt
@@ -2,20 +2,18 @@ package no.nav.sokos.ske.krav.util
 
 import java.sql.SQLException
 
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotContain
-import io.mockk.every
-import io.mockk.justRun
-import io.mockk.mockk
-import io.mockk.mockkObject
-import io.mockk.unmockkObject
-import io.mockk.verify
 import kotliquery.queryOf
-import mu.KLogger
-import mu.KotlinLogging
-import mu.Marker
+import org.slf4j.LoggerFactory
 
 import no.nav.sokos.ske.krav.config.TEAM_LOGS_MARKER
 import no.nav.sokos.ske.krav.listener.DBListener
@@ -24,34 +22,39 @@ class DBUtilsTest :
     FunSpec({
         extensions(DBListener)
 
-        val loggerMock = mockk<KLogger>(relaxed = true)
+        val dbUtilLogger = LoggerFactory.getLogger("no.nav.sokos.ske.krav.util.DBUtils") as Logger
+        val logAppender = ListAppender<ILoggingEvent>()
 
-        beforeSpec {
-            mockkObject(KotlinLogging)
-            every { KotlinLogging.logger(any<() -> Unit>()) } returns loggerMock
+        beforeTest {
+            logAppender.start()
+            dbUtilLogger.addAppender(logAppender)
+        }
+
+        afterTest {
+            dbUtilLogger.detachAppender(logAppender)
+            logAppender.stop()
         }
 
         test("transaction skal kaste exception oppover og logge error i begge vanlig log og TEAM_LOGS") {
-            justRun { loggerMock.error(any<String>()) }
-            justRun { loggerMock.error(any<Marker>(), any<String>()) }
-
             shouldThrow<SQLException> {
                 DBListener.dataSource.transaction { session ->
                     session.update(queryOf("insert into foo values(1,2)"))
                 }
             }
-            val errorMessages = mutableListOf<String>()
-            verify(exactly = 1) {
-                loggerMock.error(capture(errorMessages))
-                loggerMock.error(eq(TEAM_LOGS_MARKER), capture(errorMessages))
-            }
 
             val sqlExceptionMessage = """ERROR: relation "foo" does not exist"""
-            errorMessages.first() shouldNotContain sqlExceptionMessage
-            errorMessages.last() shouldContain sqlExceptionMessage
-        }
 
-        afterSpec {
-            unmockkObject(KotlinLogging)
+            val messages = logAppender.list
+            messages.shouldHaveSize(2)
+
+            messages.first().apply {
+                markerList.shouldBeNull()
+                formattedMessage shouldNotContain sqlExceptionMessage
+            }
+
+            messages.last().apply {
+                markerList.shouldContain(TEAM_LOGS_MARKER)
+                formattedMessage shouldContain sqlExceptionMessage
+            }
         }
     })
